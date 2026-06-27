c05693ac232d6b839a30706f1a6458d33a5036ef
RikardoFlamenguista <amantedebatata@gmail.com>
2026-06-27 14:39:06 -0300

    Dwarf generation text updated

--- Assets/Game/Scrips/Core/GI.cs
+++ Assets/Game/Scrips/Core/GI.cs
@@ -32,7 +32,7 @@ public static class GI
             case Ability_Type.REDUCE_ANOTHER_PLAYER_CARD_BY_X_POINTS:                return "Choose a Character in another player's hand. It loses 5 chips";
             case Ability_Type.STEAL_ANOTHER_PLAYER_CARD:                             return "Choose another player and steal one of their cards";
             case Ability_Type.STEAL_PLAYER_SCORE_AND_GIVE_TO_PLAYER_WITH_LESS_SCORE: return "Choose another player, steal 5 chips from them and give them to the player with the lowest score";
-            case Ability_Type.SPAWN_DWARVES_IN_PLAYER_HAND_UNTIL_ITS_FULL:           return "Choose a player and create Dwarves in their hand until it is full";
+            case Ability_Type.SPAWN_DWARVES_IN_PLAYER_HAND_UNTIL_ITS_FULL:           return "Choose a player and create a useless Dwarf in their hand";
             case Ability_Type.TURN_A_PLAYER_CARD_INTO_A_FROG:                        return "Choose another player and turn a random Character in their hand into a Frog";
             case Ability_Type.SHUFFLE_ADJACENT_CARDS:                                return "Choose a card on the table and shuffle it along with its adjacent cards";
         }
