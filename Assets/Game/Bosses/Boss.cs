fcc28790dfb83d40107e1a60e4935a49f0626a32
Arnarck <andreluismolcalais@gmail.com>
2026-08-11 18:19:00 -0300

    Added boss prefab and script

---
+++ Assets/Game/Bosses/Boss.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+
+public enum Boss_Type
+{
+    CAT,
+    WITCH,
+    KAME,
+
+    COUNT
+}
+
+
+public class Boss : MonoBehaviour
+{
+    public Boss_Type type;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
