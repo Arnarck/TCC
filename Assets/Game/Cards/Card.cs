d17d4231e876af913bf1e91f6c0b4ecea891bc77
Arnarck <andreluismolcalais@gmail.com>
2026-09-17 16:17:37 -0300

    Added base for card abilities

--- Assets/Game/Cards/Card.cs
+++ Assets/Game/Cards/Card.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
-using System.Collections;
+
 public enum Card_Type
 {
-    IMPROVE,
-    DWARF,
-    FROG,
-    PRINCESS,
-    PRINCE,
-    CARD_6,
-    CARD_7,
+    // Family 1
+    WOODEN_HOUSE_PIG,
+    PUSS_IN_BOOTS,
+    BAD_QUEEN,
+    BIG_BAD_WOLF,
+
+    // Family 2
+    STRAW_HOUSE_PIG,
+    PRINCESS_AND_FROG,
+    SLEEPING_BEAUTY,
+    UGLY_DUCK,
+    HUMAN_FROG, // Invocable
+
+    // Family 3
+    BRICK_HOUSE_PIG,
+    LITTLE_RED_RIDING_HOOD,
+    GEPETTO,
+    CINDERELLA,
+    GRANNY, // Invocable
+    PINOCCHIO, // Invocable
 
     COUNT
 }
@@ -18,50 +31,28 @@ public enum Family_Type
     FAMILY_1,
     FAMILY_2,
     FAMILY_3,
-    FAMILY_4,
 
     COUNT // Used as a way of knowing how many elements there is in this enum.
 }
-public enum CharacterType
-{
-    NONE,
-    TURTLE,
-    DOLL,
-    CAT,
-    GOLDILOCKS
-}
-public static class CharacterDatabase
-{
-    public static Family_Type GetFamily(CharacterType character)
-    {
-        switch (character)
-        {
-            case CharacterType.TURTLE:
-                return Family_Type.FAMILY_1;
 
-            case CharacterType.DOLL:
-                return Family_Type.FAMILY_2;
-
-            case CharacterType.CAT:
-                return Family_Type.FAMILY_3;
-
-            case CharacterType.GOLDILOCKS:
-                return Family_Type.FAMILY_4;
-        }
-
-        return Family_Type.FAMILY_1;
-    }
-}
 public enum Ability_Type
 {
     NONE,
-    IMPROVE_ANOTHER_CARD_BY_X_POINTS,
-    REDUCE_ANOTHER_PLAYER_CARD_BY_X_POINTS,
-    STEAL_ANOTHER_PLAYER_CARD, // @TODO: Conditions to activate card abilities.
-    STEAL_PLAYER_SCORE_AND_GIVE_TO_PLAYER_WITH_LESS_SCORE,
-    SPAWN_DWARVES_IN_PLAYER_HAND_UNTIL_ITS_FULL,
-    TURN_A_PLAYER_CARD_INTO_A_FROG,
-    SHUFFLE_ADJACENT_CARDS,
+    PROMOTE_CHARACTERS_ON_THE_RIGHT_BY_X,
+    SWAP_WITH_CHARACTER_ON_THE_RIGHT,
+    DEMOTE_CHARACTERS_ON_THE_LEFT_BY_X_AND_SELF_PROMOTE_BY_X,
+    SELF_PROMOTE_BY_X_AT_THE_END_OF_TURN,
+    PROMOTE_ADJACENT_CHARACTERS_BY_X,
+    SABOTAGE_ENEMY_CARD_ABILITY,
+    SWAP_CHARACTERS_IN_TRIO_AND_HAND,
+    PROMOTE_ALL_CHARACTERS_FROM_FAMILY_X_IN_HAND,
+    SELF_PROMOTE_BY_X_FOR_X_TURNS,
+    DEMOTE_ALL_CHARACTERS_IN_HAND_AND_TRIO_BY_X_AND_SELF_PROMOTE_BY_X_FOR_EACH,
+    SELF_PROMOTE_BY_X_AFTER_A_TRIO_SCORED_AND_SELF_DEMOTE_BY_X_AT_THE_END_OF_TURN,
+    PROMOTE_ALL_CHARACTERS_IN_TRIO_BY_X,
+    PROMOTE_ALL_CHARACTERS_ON_TABLE_BY_X,
+    SABOTAGE_OPPONENT_ABILITY,
+    DEMOTE_ADJACENT_CHARACTERS_BY_X,
 
     COUNT
 }
--- Assets/Game/Core/GI.cs
+++ Assets/Game/Core/GI.cs
@@ -16,29 +16,4 @@ public static class GI
     public static PlayerController player_card_game;
     public static PlayerControllerFirstPerson player_first_person;
     public static Boss boss;
-
-    public static string GetCardAbilityDescription(Card_Type cardType, Ability_Type abilityType)
-    {
-        switch (abilityType)
-        {
-            case Ability_Type.NONE:
-                {
-                    if      (cardType == Card_Type.DWARF) { return "This card is destroyed after being played or discarded"; }
-                    else if (cardType == Card_Type.FROG)  { return "(Requires: Princess (1)): Transform back into a Prince"; }
-
-                    Debug.LogError("No ability description created for " + cardType + " card!");
-                    return "";
-                }
-            case Ability_Type.IMPROVE_ANOTHER_CARD_BY_X_POINTS:                      return "Choose a Character in your hand. It gains 3 chips";
-            case Ability_Type.REDUCE_ANOTHER_PLAYER_CARD_BY_X_POINTS:                return "Choose a Character in another player's hand. It loses 5 chips";
-            case Ability_Type.STEAL_ANOTHER_PLAYER_CARD:                             return "Choose another player and steal one of their cards";
-            case Ability_Type.STEAL_PLAYER_SCORE_AND_GIVE_TO_PLAYER_WITH_LESS_SCORE: return "Choose another player, steal 5 chips from them and give them to the player with the lowest score";
-            case Ability_Type.SPAWN_DWARVES_IN_PLAYER_HAND_UNTIL_ITS_FULL:           return "Choose a player and create a useless Dwarf in their hand";
-            case Ability_Type.TURN_A_PLAYER_CARD_INTO_A_FROG:                        return "Choose another player and turn a random Character in their hand into a Frog";
-            case Ability_Type.SHUFFLE_ADJACENT_CARDS:                                return "Choose a card on the table and shuffle it along with its adjacent cards";
-        }
-
-        Debug.LogError("No ability description created for " + abilityType + " ability!");
-        return "";
-    }
 }
